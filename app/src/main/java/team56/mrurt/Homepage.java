78595abf600efea6a6dbf5e38d3966a4e1b18049
cwyck95 <cwyckoff3@gatech.edu>
2016-02-15 17:10:14 -0500

    Update Homepage.java

--- app/src/main/java/team56/mrurt/Homepage.java
+++ app/src/main/java/team56/mrurt/Homepage.java
@@ -1,24 +1,43 @@
-package team56.mrurt;
+package mrurt.team56.movierecommender;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 /**
- * Created by alexlabanowski on 2/10/16.
+ * HomePage
  */
-public class Homepage extends Activity {
+public class HomePage extends AppCompatActivity {
+
+    public static UserStorage mUserStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.activity_home_page);
+
+        mUserStorage = new UserStorage();
+
     }
 
+    /**
+     * returns the user to the welcome screen after logging them out
+     * @param view The view
+     */
     public void logout(View view){
         Intent intent = new Intent(this, Welcome.class);
         startActivity(intent);
         finish();
     }
+
+
+    /**
+     * Returns the UserStorage
+     * @return mUserStorage
+     */
+    public UserStorage getUserStorage() {
+        return mUserStorage;
+    }
 }
+
