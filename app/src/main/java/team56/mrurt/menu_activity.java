316957737bb8ffc49817c18905cb889cb4f59cab
Natalie <castro.natalie94@gmail.com>
2016-02-21 17:10:20 -0500

    Added Menu and modified XML Files

---
+++ app/src/main/java/team56/mrurt/menu_activity.java
@@ -0,0 +1,24 @@
+package team56.mrurt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by Haruka on 2016/02/21.
+ */
+public class menu_activity extends AppCompatActivity {
+
+    public void logout(MenuItem item){
+        Intent intent = new Intent(this, Welcome.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void viewProfile(MenuItem item){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
