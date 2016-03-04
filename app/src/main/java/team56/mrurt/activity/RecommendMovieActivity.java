2eceb644bce87b6723807ea87571f02a279d4c9c
Natalie <castro.natalie94@gmail.com>
2016-03-04 16:13:00 -0500

    Made Recommender Class, made buttons

--- app/src/main/java/team56/mrurt/activity/RecommendMovieActivity.java
+++ app/src/main/java/team56/mrurt/activity/RecommendMovieActivity.java
@@ -1,7 +1,13 @@
 package team56.mrurt.activity;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Haruka on 2016/03/04.
  */
-public class RecommendMovieActivity {
+
+/*
+* Sorts Movies into a list by specific Criteria
+ */
+public class RecommendMovieActivity extends AppCompatActivity{
 }
